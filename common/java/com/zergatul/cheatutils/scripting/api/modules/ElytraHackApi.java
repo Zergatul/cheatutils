ff82b5b2c42f95a11035fd09ac77eabe3d4ce515
Igor Budzhak <z3rgatul@gmail.com>
2023-06-21 13:51:06 +0300

    mixins can fail without crash

---
+++ common/java/com/zergatul/cheatutils/scripting/api/modules/ElytraHackApi.java
@@ -0,0 +1,12 @@
+package com.zergatul.cheatutils.scripting.api.modules;
+
+import com.zergatul.cheatutils.configs.ConfigStore;
+import com.zergatul.cheatutils.configs.ElytraHackConfig;
+
+public class ElytraHackApi extends ModuleApi<ElytraHackConfig> {
+
+    @Override
+    protected ElytraHackConfig getConfig() {
+        return ConfigStore.instance.getConfig().elytraHackConfig;
+    }
+}
