227e9467bb153e3d548114c7a577e2d5a7138a06
Igor Budzhak <z3rgatul@gmail.com>
2024-03-17 23:17:07 +0200

    added new elytraFly API methods

--- common/java/com/zergatul/cheatutils/scripting/modules/ElytraHackApi.java
+++ common/java/com/zergatul/cheatutils/scripting/modules/ElytraHackApi.java
@@ -5,6 +5,39 @@ import com.zergatul.cheatutils.configs.ElytraHackConfig;
 
 public class ElytraHackApi extends ModuleApi<ElytraHackConfig> {
 
+    public double getMaxSpeed() {
+        return getConfig().maxSpeed;
+    }
+
+    public double getVerticalAcceleration() {
+        return getConfig().vanillaFlyVerticalAcceleration;
+    }
+
+    public double getHorizontalAcceleration() {
+        return getConfig().vanillaFlyHorizontalAcceleration;
+    }
+
+    public void setMaxSpeed(double speed) {
+        var config = getConfig();
+        getConfig().maxSpeed = speed;
+        config.validate();
+        ConfigStore.instance.requestWrite();
+    }
+
+    public void setVerticalAcceleration(double acceleration) {
+        var config = getConfig();
+        getConfig().vanillaFlyVerticalAcceleration = acceleration;
+        config.validate();
+        ConfigStore.instance.requestWrite();
+    }
+
+    public void setHorizontalAcceleration(double acceleration) {
+        var config = getConfig();
+        getConfig().vanillaFlyHorizontalAcceleration = acceleration;
+        config.validate();
+        ConfigStore.instance.requestWrite();
+    }
+
     @Override
     protected ElytraHackConfig getConfig() {
         return ConfigStore.instance.getConfig().elytraHackConfig;
