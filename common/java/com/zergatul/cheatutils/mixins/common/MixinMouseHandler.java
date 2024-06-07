e54342b63b5da5f896349632cd7097757cc85a29
Igor Budzhak <z3rgatul@gmail.com>
2024-06-07 06:44:28 +0300

    compatibility fixes for freecam/zoom

--- common/java/com/zergatul/cheatutils/mixins/common/MixinMouseHandler.java
+++ common/java/com/zergatul/cheatutils/mixins/common/MixinMouseHandler.java
@@ -1,9 +1,6 @@
 package com.zergatul.cheatutils.mixins.common;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalDoubleRef;
 import com.zergatul.cheatutils.common.Events;
-import com.zergatul.cheatutils.modules.visuals.Zoom;
 import com.zergatul.cheatutils.common.events.MouseScrollEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
@@ -15,24 +12,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MouseHandler.class)
-public class MixinMouseHandler {
+public abstract class MixinMouseHandler {
 
     @Shadow
     @Final
     private Minecraft minecraft;
-
-    @Inject(method = "turnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/tutorial/Tutorial;onMouse(DD)V", shift = At.Shift.AFTER))
-    private void onLocalPlayerTurn(CallbackInfo info, @Local(index = 11) LocalDoubleRef yRotRef, @Local(index = 13) LocalDoubleRef xRotRef) {
-        // 5 7
-        if (Zoom.instance.isActive()) {
-            double xRot = xRotRef.get();
-            double yRot = yRotRef.get();
-            xRot *= Zoom.instance.getFovFactor();
-            yRot *= Zoom.instance.getFovFactor();
-            xRotRef.set(xRot);
-            yRotRef.set(yRot);
-        }
-    }
 
     @Inject(
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isSpectator()Z"),
