4521764aaef098aa342d1b4b4521bd15244df6eb
Igor Budzhak <z3rgatul@gmail.com>
2024-09-29 21:22:04 +0300

    forge inv move

--- forge/src/main/java/com/zergatul/cheatutils/mixins/forge/MixinKeyMapping.java
+++ forge/src/main/java/com/zergatul/cheatutils/mixins/forge/MixinKeyMapping.java
@@ -1,19 +1,35 @@
 package com.zergatul.cheatutils.mixins.forge;
 
-import com.zergatul.cheatutils.configs.ConfigStore;
+import com.zergatul.cheatutils.modules.hacks.InvMove;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.extensions.IForgeKeyMapping;
+import net.minecraftforge.client.settings.IKeyConflictContext;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = KeyMapping.class, remap = false)
 public abstract class MixinKeyMapping implements IForgeKeyMapping {
 
-    @Override
-    public boolean isConflictContextAndModifierActive() {
-        if (ConfigStore.instance.getConfig().invMoveConfig.enabled) {
-            return true;
-        } else {
-            return IForgeKeyMapping.super.isConflictContextAndModifierActive();
+    @Inject(
+            method = "getKeyConflictContext",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void onGetKeyConflictContext(CallbackInfoReturnable<IKeyConflictContext> info) {
+        if (InvMove.instance.shouldPassEvents(Minecraft.getInstance().screen)) {
+            info.setReturnValue(new IKeyConflictContext() {
+                @Override
+                public boolean isActive() {
+                    return true;
+                }
+
+                @Override
+                public boolean conflicts(IKeyConflictContext other) {
+                    return false;
+                }
+            });
         }
     }
 }
--- forge/src/main/java/com/zergatul/cheatutils/mixins/forge/MixinKeyboardHandler.java
+++ forge/src/main/java/com/zergatul/cheatutils/mixins/forge/MixinKeyboardHandler.java
@@ -1,15 +1,41 @@
 package com.zergatul.cheatutils.mixins.forge;
 
+import com.zergatul.cheatutils.modules.hacks.InvMove;
 import net.minecraft.client.KeyboardHandler;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(KeyboardHandler.class)
 public abstract class MixinKeyboardHandler {
 
-    /*@Redirect(
-            method = "keyPress(JIIII)V",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;screen:Lnet/minecraft/client/gui/screens/Screen;", opcode = Opcodes.GETFIELD, ordinal = 4))
-    private Screen onCheckScreenPassEvents(Minecraft mc) {
-        return InvMove.instance.overrideGetScreen(mc);
-    }*/
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+
+    @ModifyVariable(
+            method = "keyPress",
+            at = @At(value = "LOAD", ordinal = 1),
+            ordinal = 0)
+    private boolean[] onModifyScreenKeyState(boolean[] state) {
+        if (InvMove.instance.shouldPassEvents(this.minecraft.screen)) {
+            state[0] = false;
+        }
+        return state;
+    }
+
+    @ModifyVariable(
+            method = "keyPress",
+            at = @At(value = "LOAD", ordinal = 2),
+            index = 10)
+    private boolean onModifyNoScreen(boolean noScreen) {
+        if (InvMove.instance.shouldPassEvents(this.minecraft.screen)) {
+            return true;
+        } else {
+            return noScreen;
+        }
+    }
 }
