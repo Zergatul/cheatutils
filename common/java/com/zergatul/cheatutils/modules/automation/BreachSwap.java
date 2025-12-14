cd5ae327e296331f7fa7b60b9b2335a1a31d74c4
someRandomDude-a <abhyuditac+github@gmail.com>
2025-12-14 12:56:57 +0530

    modified breachSwap inventory search to use tags

--- common/java/com/zergatul/cheatutils/modules/automation/BreachSwap.java
+++ common/java/com/zergatul/cheatutils/modules/automation/BreachSwap.java
@@ -11,9 +11,10 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import com.zergatul.cheatutils.scripting.Root;
-
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.player.Inventory;
-import com.zergatul.cheatutils.scripting.types.ItemStackWrapper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
 
 public class BreachSwap implements Module {
 
@@ -37,7 +38,7 @@ public class BreachSwap implements Module {
             return;
         }
 
-        if(!Root.input.isKeyDown(config.triggerKey)){
+        if(!Root.input.isKeyDown(config.triggerKey)) {
             attacked = false;
             return;
         }
@@ -67,24 +68,25 @@ public class BreachSwap implements Module {
             int sword = -1;
             int mace = -1;
             int weapon;
-            Inventory invent = mc.player.getInventory();
-            for(int i = 0; i < 9; i++){
-                ItemStackWrapper item = new ItemStackWrapper(invent.getItem(i));
-                if(item.hasEnchantment("breach"))mace = i;
-                String itemID = item.getItem().getId();
-                if(itemID.endsWith("axe"))axe = i;
-                else if(itemID.endsWith("sword"))sword = i;
+            Inventory inventory = mc.player.getInventory();
+
+            for(int i = 0; i < 9; i++) {
+                ItemStack item = inventory.getItem(i);
+                if(item.getTags().anyMatch(tag -> tag == ItemTags.SWORDS))sword = i; 
+                else if(item.getTags().anyMatch(tag -> tag == ItemTags.AXES))axe = i;
+                else if(item.getEnchantments().keySet().stream().anyMatch(enchantment -> enchantment.value().effects().keySet().contains(EnchantmentEffectComponents.ARMOR_EFFECTIVENESS)))mace = i;
             }
-            if(mace == -1){
+
+            if(mace == -1) {
                 return;
             }
 
-            if(axe == -1 && sword == -1){
+            if(axe == -1 && sword == -1) {
                 return;
             }
 
-            if(config.useAxe){//Prefer Axe over sword
-                if(axe != -1){
+            if(config.useAxe) {//Prefer Axe over sword
+                if(axe != -1) {
                     weapon = axe;
                 }
                 else {
@@ -92,7 +94,7 @@ public class BreachSwap implements Module {
                 }
             }
             else {
-                if(sword != -1){
+                if(sword != -1) {
                     weapon = sword;
                 }
                 else {
@@ -100,11 +102,11 @@ public class BreachSwap implements Module {
                 }
             }
 
-            if(config.breakShield){
+            if(config.breakShield) {
                 if(Root.game.entities.isUsingItemWithOffHand(entity.getId()) && Root.game.entities.getEquippedOffHandItem(entity.getId()).getItem().getId().equals("minecraft:shield")){
 
-                    if(axe != -1){
-                        invent.setSelectedSlot(axe);
+                    if(axe != -1) {
+                        inventory.setSelectedSlot(axe);
                         mc.gameMode.attack(mc.player, entity);
                         mc.player.swing(InteractionHand.MAIN_HAND);
                     }
@@ -114,10 +116,10 @@ public class BreachSwap implements Module {
             }
 
             attacked = true;
-            invent.setSelectedSlot(mace);
+            inventory.setSelectedSlot(mace);
             mc.gameMode.attack(mc.player, entity);
             mc.player.swing(InteractionHand.MAIN_HAND);
-            invent.setSelectedSlot(weapon);
+            inventory.setSelectedSlot(weapon);
         }
     }
 }
