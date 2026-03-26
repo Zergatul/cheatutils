e63e6c55fdeca831b17d125ad43e9a5542f9a989
Igor Budzhak <z3rgatul@gmail.com>
2026-03-27 00:28:45 +0200

    add cf build

--- fabric/src/main/java/com/zergatul/cheatutils/wrappers/ModEnvironment.java
+++ fabric/src/main/java/com/zergatul/cheatutils/wrappers/ModEnvironment.java
@@ -1,11 +1,16 @@
 package com.zergatul.cheatutils.wrappers;
 
 import com.zergatul.cheatutils.ModMain;
+import com.zergatul.cheatutils.utils.ResourceHelper;
 import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ModEnvironment {
 
     public static final boolean IS_PRODUCTION = !FabricLoader.getInstance().isDevelopmentEnvironment();
+    public static final boolean IS_CURSEFORGE_RESTRICTED = isCurseForgeRestricted();
 
     public static String getModLoader() {
         return "Fabric";
@@ -21,5 +26,17 @@ public class ModEnvironment {
 
     public static int getModCount() {
         return FabricLoader.getInstance().getAllMods().size();
+    }
+
+    private static boolean isCurseForgeRestricted() {
+        InputStream stream = ResourceHelper.get("curse-forge-build");
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException _) {}
+            return true;
+        } else {
+            return false;
+        }
     }
 }
