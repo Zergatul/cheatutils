2aef69dc35b3d9db7c83ec21816c6b94ebc78bd9
Igor Budzhak <z3rgatul@gmail.com>
2026-06-12 00:01:21 +0300

    fabric test setup

---
+++ fabric/src/test/java/com/zergatul/cheatutils/fabric/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.zergatul.cheatutils.fabric;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void helloWorld() {
+        assertEquals("Hello, world!", "Hello, world!");
+    }
+}
