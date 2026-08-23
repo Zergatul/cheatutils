84a06cd7cdf7c6c27adeb2152deda9161f421b37
Igor Budzhak <z3rgatul@gmail.com>
2026-08-23 20:03:01 +0300

    fixed Crystal Aura crash when enabled from script

--- common/java/com/zergatul/cheatutils/scripting/modules/CrystalAuraApi.java
+++ common/java/com/zergatul/cheatutils/scripting/modules/CrystalAuraApi.java
@@ -2,8 +2,15 @@ package com.zergatul.cheatutils.scripting.modules;
 
 import com.zergatul.cheatutils.configs.ConfigStore;
 import com.zergatul.cheatutils.configs.CrystalAuraConfig;
+import com.zergatul.cheatutils.modules.hacks.CrystalAura;
 
 public class CrystalAuraApi extends ModuleApi<CrystalAuraConfig> {
+
+    @Override
+    protected void onEnableChanged() {
+        super.onEnableChanged();
+        CrystalAura.instance.onEnableStateChanged();
+    }
 
     @Override
     protected CrystalAuraConfig getConfig() {
