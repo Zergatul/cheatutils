81133c47a0a43a7ce3af5ee4ddcaaee32d393b50
Igor Budzhak <z3rgatul@gmail.com>
2026-09-06 01:34:03 +0300

    fixed chunk culling in FreeCam with Sodium

--- common/java/net/caffeinemc/mods/sodium/client/render/SodiumWorldRenderer.java
+++ common/java/net/caffeinemc/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -4,5 +4,5 @@ import net.minecraft.client.Camera;
 import org.joml.Matrix4f;
 
 public class SodiumWorldRenderer {
-    public void setupTerrain(Camera camera, Object viewport, Object fogParameters, boolean spectator, boolean updateChunksImmediately, Matrix4f cullMatrix) {}
+    public void setupTerrain(Camera camera, Object viewport, Object fogParameters, boolean useOcclusionCulling, boolean updateChunksImmediately, Matrix4f cullMatrix) {}
 }
